5c50473957c40a8c9698cb993905156bc6754ae0
KimJinSu-Git <creeper31109@gmail.com>
2025-06-28 17:35:46 +0900

    0.51 Version
    Title Scene 임시 완료
    이제 인 게임에서 ESC 버튼을 눌러 게임을 종료할지 타이틀 씬으로 돌아갈지 등의 UI만 추가하고 마지막 구간 적 배치하고 끝내자.

--- Assets/Assets/Dark - Complete Horror UI/Scripts/Events/PressKeyEvent.cs
+++ Assets/Assets/Dark - Complete Horror UI/Scripts/Events/PressKeyEvent.cs
@@ -1,25 +1,26 @@
-﻿// using UnityEngine;
-// using UnityEngine.Events;
-//
-// namespace Michsky.UI.Dark
-// {
-//     public class PressKeyEvent : MonoBehaviour
-//     {
-//         // Settings
-//         public InputAction hotkey;
-//
-//         // Events
-//         public UnityEvent onPressEvent;
-//
-//         void Start()
-//         {
-//             hotkey.Enable();
-//         }
-//
-//         void Update()
-//         {
-//             if (hotkey.triggered)
-//                 onPressEvent.Invoke();
-//         }
-//     }
-// }
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Michsky.UI.Dark
+{
+    public class PressKeyEvent : MonoBehaviour
+    {
+        // Settings
+        public InputAction hotkey;
+
+        // Events
+        public UnityEvent onPressEvent;
+
+        void Start()
+        {
+            hotkey.Enable();
+        }
+
+        void Update()
+        {
+            if (hotkey.triggered)
+                onPressEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/ETC/LoadChapter0.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadChapter0 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
